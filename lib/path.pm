07c91acb5ddeb8e83f40cd18f1f85a1653724a42
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-05 06:06:39 +0000

    Orion commit to www.iconoclasts.blog by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@21976 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -128,7 +128,7 @@ walk_content_tree {
     open my $fh, "<:raw", "lib/acl.yml" or die "Can't open acl.yml: $!";
     push @acl, @{Load join "", <$fh>};
     for (glob("content/*/index.md.*")) {
-        s!/index\.md\.[^/]+$!!:
+        s!/index\.md\.[^/]+$!!;
 		push @acl, { path => $_, rules => {
              '@bloggers'  => 'rw',
              '@svnadmin' => 'rw',
