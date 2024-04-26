c7267e1128038ae4329e94141b57f86c8472c0b9
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-26 15:20:46 +0000

    SunStar Systems Orion backed www.iconoclasts.blog sources successfully built.
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@24243 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/joe/triple-products.page/bibliography.yml.sv
+++ content/joe/triple-products.page/bibliography.yml.sv
@@ -1,10 +1,28 @@
 ---
-archived: ~
-categories: ~
+title: Bibliography
+status: generated
 dependencies: ../triple-products.bib.sv
-keywords: ~
-published: ~
-status: genererad
-title: Bibliografi
 ---
-
+- Författare: Knapp, Anthony W
+  Journal: Journal of Functional Analysis
+  Titel: TernÃ¤re positiv definitiv quad\-ratische Formen mit gleichen Darstellungs\-zahlen
+  Utgivare: Princeton University Press
+  archivePrefix: arXiv
+  datum: 2023-09-01
+  doi: https://doi.org/10.1016/j.jfa.2022.109404
+  eprint: '1810.01024'
+  författare: Zhou, Gengqiang
+  id: MS67
+  journal: Nach\-richten der KÃ¶nig\-lichen Gesell\-schaft der Wissen\-schaften zu
+    GÃ¶ttingen
+  nummer: '1'
+  publisher: Springer Science \&amp; Business Media
+  sidor: 187--200
+  tidskrift: Annaler av matematik
+  titel: Riemanniska beläggningar och isospektrala grenrör
+  title: Kompakthet hos isospektrala kompakta grenrör med avgränsade krökar
+  type: article
+  url: https://www.tobias-franke.eu/log/2017/04/19/triple-products.html
+  utgivare: Bonner Mathe\matische Schriften
+  volym: '181'
+  år: '1997'
