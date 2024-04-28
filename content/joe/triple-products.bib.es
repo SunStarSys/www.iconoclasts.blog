aa5ca710ece40835517798edd035904537a049be
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-28 00:23:57 +0000

    freedom
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@24479 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/joe/triple-products.bib.es
+++ content/joe/triple-products.bib.es
@@ -1,14 +1,14 @@
 @article{MS67,
-  title=La curvatura y los valores propios de la Laponia,
-  journal={Diario de geometría diferencial},
+  title={La curvatura y los valores propios de la Laponia},
+  journal={Diario de Geometría Diferencial},
   author={McKean, Henry P. y Singer, Isadore M.},
   volume={1},
   number={1-2},
   pages={43--69},
   year={1967},
-  publisher={Universidad Lehigh}
+  publisher={Universidad de Lehigh}
 }
-
+}
 @book{JC19,
   author={Conway, John B.},
   title={Un curso de análisis funcional},
@@ -16,59 +16,59 @@
   year={2019},
   publisher={Springer}
 }
-
+}
 @article{CS92,
  author={Conway, John H y Sloane, N. J. A.},
- title={Pantallas de cuatro dimensiones con la misma serie theta},
- journal={Internat. Matemáticas Res. Avisos,
+ title={Rejillas de cuatro dimensiones con la misma serie theta},
+ journal={Internat. Matemáticas Res. Avisos},
  volume={4},
  year={1992},
  pages={93--96}
 }
-
+}
 @article{DH11,
-  author=Datchev, Kiril y Hezari, Hamid,
+  author={Datchev, Kiril y Hezari, Hamid},
   title={Problemas inversos en geometría espectral},
   journal={Problemas y aplicaciones inversos: Inside Out II},
   volume={60},
   year={2011},
   pages={455--486}
 }
-
+}
 @book{FBZ04,
- author=Frenkel, Edward y Ben-Zvi, David,
- title={Vertex álgebras y curvas algebraicas},
+ author={Frenkel, Edward y Ben-Zvi, David},
+ title={Álgebras de vértice y curvas algebraicas},
  volume={88},
  publisher={American Mathematical Soc.},
  year={2004}
 }
-
+}
 @online{TF17,
   author={Franke, Tobias},
-  title={Triple prod\-uct int\grals},
+  title={Triple prod\-uct int\-grals},
   date={2023-09-01},
   url={https://www.tobias-franke.eu/log/2017/04/19/triple-products.html}
 }
-
+}
 @article{CG93,
   author={Gordon, Carolyn},
-  title={Manifolds Riemannian cerrados isospectrales que no son isométricos localmente},
-  journal={Diario de geometría diferencial},
+  title={Manifestaciones isospectrales cerradas de Riemann que no son isométricas localmente},
+  journal={Diario de Geometría Diferencial},
   volume={37},
   number={3},
   year={1993},
   pages={639--649}
 }
-
+}
 @article{GK80,
   author={Guillemin, Victor y Kazhdan, David},
-  title={Algunos resultados espectrales inversos para n-manifolds curvados negativamente},
-  journal={Procedimientos de Simposios en Matemáticas Puras},
+  title={Algunos resultados espectrales inversos para n-manifolds curvado negativamente},
+  journal={Actas de Simposio en Matemáticas Puras},
   volume={36},
   pages={301--312},
   year={1980}
 }
-
+}
 @article{LH68,
   author={Hörmander, Lars},
   title={La función espectral de un operador elíptico},
@@ -77,22 +77,22 @@
   year={1968},
   pages={217--242}
 }
-
+}
 @book{AK01,
   author={Knapp, Anthony W},
   title={Teoría de la representación de grupos semisímiles: una visión general basada en ejemplos},
-  publisher={Princeton University Press},
+  publisher={Prensa Universidad de Princeton},
   year={2001}
 }
-
+}
 @online{LS18,
   author={Lu, Jianfeng y Steinerberger, Stefan},
-  title={En cuanto a los productos puntuales de las autofunciones elípticas},
+  title={Sobre productos puntuales de autofunciones elípticas},
   archivePrefix={arXiv},
-  eprint={1810,01024},
+  eprint={1810.01024},
   year={2018}
 }
-
+}
 @proceedings{JM64,
   author={Milnor, John},
   title={Valores propios del operador de Laplace en determinados colectores},
@@ -102,71 +102,71 @@
   year={1964},
   pages={542--542}
 }
-
+}
 @article{NRR22,
   author={Nilsson, Erik y Rowlett, Julie y Rydell, Felix},
-  title={El problema isospectral para el tori plano desde tres perspectivas},
-  journal=Boletín de la Sociedad Matemática Americana,
+  title={El problema isospectral del tori plano desde tres perspectivas},
+  journal={Boletín de la Sociedad Americana de Matemáticas},
   volume={60},
   number={1},
   year={2023},
   pages={39--83}
 }
-
+}
 @book{RS75,
-  author=Reed, Michael y Simon, Barry,
+  author={Reed, Michael y Simon, Barry},
   title={Métodos de la física matemática moderna II: Análisis de Fourier, autoconjunción},
   volume={2},
   publisher={Elsevier},
   year={1975}
 }
-
+}
 @article{PS94,
   author={Sarnak, Peter},
-  title={Integrales de productos de funciones propias},
-  journal={IMRN},
+  title={Integrales de los productos de las funciones propias},
+  journal={NoHaCC},
   volume={6},
   year={1994},
   pages={251--260}
 }
-
+}
 @phdthesis{AS94,
   author={Schiemann, Alexander},
-  title={Ternäre positiv definida quad\-ratische Formen mit gleichen Darstellungs\-zahlen},
+  title={Ternäre positiv definido quad\-ratische Formen mit gleichen Darstellungs\-zahlen},
   publisher={Bonner Mathe\matische Schriften},
-  school={Universität Bonn},
+  school={Universidad de Bonn},
   volume={268},
   year={1993}
 }
-
+}
 @article{TS85,
   author={Sunada, Toshikazu},
-  title={Riemannian cubre y manifolds isospectrales},
+  title={Recubrimientos riemannianos y colectores isospectrales},
   journal={Anales de Matemáticas},
   volume={121},
   number={1},
   year={1985},
   pages={169--186}
 }
-
+}
 @article{ST80,
   author={Tanno, Shukichi},
   title={Caracterización de las esferas canónicas por el espectro},
-  journal={Math. Z.},
+  journal={Matemáticas Z.},
   volume={175},
   number={3},
   year={1980},
   pages={267--274}
 }
-
+}
 @book{MT13,
-  author=Taylor, Michael,
-  title=Ecuaciones diferenciales parciales II: estudios cualitativos de ecuaciones lineales,
+  author={Taylor, Michael},
+  title={Ecuaciones diferenciales parciales II: estudios cualitativos de ecuaciones lineales},
   volume={116},
-  publisher={Springer Science \&amp; Business Media},
+  publisher={Springer Science \&amp; Medios empresariales},
   year={2013}
 }
-
+}
 @article{HW11,
   author={Weyl, Hermann},
   title={Ãber die asymp\-totische Ver\-teilung der Eigen\-werte},
@@ -174,23 +174,23 @@
   year={1911},
   pages={110--117}
 }
-
+}
 @article{EW22,
   author={Wyman, Emmett L},
-  title={Triángulos y productos triples de las funciones propias de Laplace},
-  journal={Asiento de análisis funcional},
+  title={Triángulos y triples productos de las funciones propias de Laplace},
+  journal={Diario de Análisis Funcional},
   volum={282},
   number={8},
   year={2022},
   doi = {https://doi.org/10.1016/j.jfa.2022.109404}
 }
-
+}
 @article{GZ97,
   author={Zhou, Gengqiang},
   title={Compactitud de los colectores compactos isospectrales con curvaturas delimitadas},
-  journal=El Pacífico J. Matemáticas,
+  journal={Pacífico J. Matemáticas},
   volume={181},
   number={1},
   year={1997},
-  pages={187--200}
+  pages={187-200}
 }
--- content/joe/triple-products.bib.sv
+++ content/joe/triple-products.bib.sv
@@ -1,196 +1,196 @@
 @article{MS67,
-  title=Korvatur och Lapplands egenvärden
-  journal=Differentialgeometri,
+  title={Kurvatur och Lapplands egenvärden},
+  journal={Tidskrift för differentialgeometri},
   author={McKean, Henry P. och Singer, Isadore M.},
   volume={1},
   number={1-2},
-  pages={43--69},
+  pages={43-69},
   year={1967},
   publisher={Lehigh University}
 }
-
+}
 @book{JC19,
-  author=Konway, John B.
+  author={Conway, John B.},
   title={En kurs i funktionell analys},
   volume={96},
   year={2019},
   publisher={Springer}
 }
-
+}
 @article{CS92,
- author=Conway, John H och Sloane, N. J. A.
- title={Fyra-dimensionella galler med samma teta-serie},
- journal={Internat. Matematik. Res. Meddelanden
+ author={Conway, John H och Sloane, N. J. A.},
+ title={Fyrdimensionella galler med samma teta-serie},
+ journal={Internat Matematik. Res. Meddelanden},
  volume={4},
  year={1992},
- pages={93--96}
+ pages={93-96}
 }
-
+}
 @article{DH11,
-  author=Datchev, Kiril och Hezari, Hamid
-  title=Omvända problem i spektralgeometri
-  journal={Inverse problems and applications: Inside Out II},
+  author={Datchev, Kiril och Hezari, Hamid},
+  title={Omvända problem i spektralgeometri},
+  journal={Inversa problem och tillämpningar: Inside Out II},
   volume={60},
   year={2011},
-  pages={455--486}
+  pages={455-486}
 }
-
+}
 @book{FBZ04,
- author=Frenkel, Edward och Ben-Zvi, David
+ author={Frenkel, Edward och Ben-Zvi, David},
  title={Vertex algebror och algebraiska kurvor},
  volume={88},
- publisher=Amerikansk matematisk soc.
+ publisher={Amerikansk matematisk soc.},
  year={2004}
 }
-
+}
 @online{TF17,
   author={Franke, Tobias},
-  title={Triple prod\-uct inte\-grals},
+  title={Heltal för trippelprod\ukt},
   date={2023-09-01},
   url={https://www.tobias-franke.eu/log/2017/04/19/triple-products.html}
 }
-
+}
 @article{CG93,
   author={Gordon, Carolyn},
-  title={Isospectral stängda Riemannian grenrör som inte är lokalt isometriska},
-  journal=Differentialgeometri,
+  title={Isospektral stängda Riemanniska grenrör som inte är lokalt isometriska},
+  journal={Tidskrift för differentialgeometri},
   volume={37},
   number={3},
   year={1993},
-  pages={639--649}
+  pages={639-649}
 }
-
+}
 @article{GK80,
-  author=Guillemin, Victor och Kazhdan, David
+  author={Guillemin, Victor och Kazhdan, David},
   title={Vissa inversa spektrala resultat för negativt böjda n-mångfalder},
-  journal=Symposier i ren matematik
+  journal={Symposier i ren matematik},
   volume={36},
-  pages={301--312},
+  pages={301-312},
   year={1980}
 }
-
-@article{LH68,
-  author=Lars,
-  title=Den spektrala funktionen hos en elliptisk operatör,
-  journal={Matematik tidigare och nuvarande Fourier Integral Operators},
-  publisher=Hotell nära Berlin Heidelberg
-  year={1968},
-  pages={217--242}
 }
-
+@article{LH68,
+  author={Hörmander, Lars},
+  title={Den spektrala funktionen hos en elliptisk operatör},
+  journal={Matematik Tidigare och nuvarande Fourier Integral Operatorer},
+  publisher={Springer Berlin Heidelberg},
+  year={1968},
+  pages={217-242}
+}
+}
 @book{AK01,
   author={Knapp, Anthony W},
-  title={Representationsteori för semisimpletta grupper: en översikt baserad på exempel},
-  publisher=Hotell nära Princeton University Press
+  title={Representationsteori för semisimplegrupper: en översikt baserad på exempel},
+  publisher={Hotell nära Princeton University Press},
   year={2001}
 }
-
+}
 @online{LS18,
-  author=Lu, Jianfeng och Steinerberger, Stefan
+  author={Lu, Jianfeng och Steinerberger, Stefan},
   title={På punktvisa produkter av elliptiska egenfunktioner},
   archivePrefix={arXiv},
   eprint={1810.01024},
   year={2018}
 }
-
+}
 @proceedings{JM64,
-  author=Milnor, John
-  title={Egenvärden för Laplace-operatorn på vissa grenrör},
-  journal=för National Academy of Sciences,
+  author={Milnor, John},
+  title={Egenvärden för Laplace-operatören på vissa grenrör},
+  journal={Förfaranden vid Vetenskapsakademien},
   volume={51},
   number={4},
   year={1964},
-  pages={542--542}
+  pages={542-542}
 }
-
+}
 @article{NRR22,
-  author={Nilsson, Erik och Rowlett, Julie och Rydell, Felix}
-  title=Det isospektrala problemet för platt tori ur tre perspektiv;
-  journal=från American Mathematical Society,
+  author={Nilsson, Erik och Rowlett, Julie och Rydell, Felix},
+  title={Isospektralt problem för platt tori ur tre perspektiv},
+  journal={Kommuniké från American Mathematical Society},
   volume={60},
   number={1},
   year={2023},
-  pages={39--83}
+  pages={39-83}
 }
-
+}
 @book{RS75,
-  author=Reed, Michael och Simon, Barry
-  title={Metoder för modern matematisk fysik II: Fourieranalys, självstyre},
+  author={Reed, Michael och Simon, Barry},
+  title={Metoder för modern matematisk fysik II: Fourieranalys, självförverkligande},
   volume={2},
   publisher={Elsevier},
   year={1975}
 }
-
+}
 @article{PS94,
-  author=Sarnak, Peter,
+  author={Sarnak},
   title={Integraler av produkter av egenfunktioner},
   journal={IMRN},
   volume={6},
   year={1994},
-  pages={251--260}
+  pages={251-260}
 }
-
+}
 @phdthesis{AS94,
-  author=Schiemann, Alexander
+  author={Schiemann},
   title={TernÃ¤re positiv definitiv quad\-ratische Formen mit gleichen Darstellungs\-zahlen},
-  publisher={Bonner Mathe\matische Schriften}
-  school=Universität Bonn
+  publisher={Bonner Mathe\matische Schriften},
+  school={Universität Bonn},
   volume={268},
   year={1993}
 }
-
+}
 @article{TS85,
-  author=Solada, Toshikazu
-  title={Riemannbeläggningar och isospektralmångfalder},
-  journal=Annorlunda matematik,
+  author={Sunada (ort)},
+  title={Riemannbeläggningar och isospektrala grenrör},
+  journal={Matematikens årtal},
   volume={121},
   number={1},
   year={1985},
   pages={169-186}
 }
-
+}
 @article{ST80,
-  author={Tanno, Shukichi}
+  author={Tanno, Shukichi},
   title={En karakterisering av de kanoniska sfärerna av spektrumet},
-  journal={Math. Z.},
+  journal={Matematik. Ö.},
   volume={175},
   number={3},
   year={1980},
-  pages={267--274}
+  pages={267-274}
 }
-
+}
 @book{MT13,
   author={Taylor, Michael},
   title={Partiella differentialekvationer II: kvalitativa studier av linjära ekvationer},
   volume={116},
-  publisher={Springer Science \&amp; Business Media},
+  publisher={Springer Science \&amp; Affärsmedia},
   year={2013}
 }
-
+}
 @article{HW11,
-  author={Weyl, Hermann}
+  author={Weyl, Hermann},
   title={Ãber die asymp\-totische Ver\-teilung der Eigen\-werte},
   journal={Nach\-richten der KÃ¶nig\-lichen Gesell\-schaft der Wissen\-schaften zu GÃ¶ttingen},
   year={1911},
-  pages={110--117}
+  pages={110-117}
 }
-
+}
 @article{EW22,
   author={Wyman, Emmett L},
   title={Trianglar och trippelprodukter av Laplace egenfunktioner},
-  journal={Journal of Functional Analysis},
+  journal={Verifikation för funktionell analys},
   volum={282},
   number={8},
   year={2022},
   doi = {https://doi.org/10.1016/j.jfa.2022.109404}
 }
-
+}
 @article{GZ97,
   author={Zhou, Gengqiang},
-  title={Kompakthet av isospektrala kompakta grenrör med avgränsade krökar},
-  journal={Pacific J. Matematik},
+  title={Kompakthet hos isospektrala kompakta grenrör med avgränsade krökar},
+  journal={Stilla havet J. Matematik},
   volume={181},
   number={1},
   year={1997},
-  pages={187--200}
+  pages={187-200}
 }
