c54faddba3be2df34e028ee4eac75c2cbb4bd0f9
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-11 22:56:08 +0000

    Orion commit to www.iconoclasts.blog by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@28653 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- templates/main.html.sv
+++ templates/main.html.sv
@@ -39,182 +39,55 @@
           <span class="navbar-toggler-icon"></span>
       </button>
 
-<div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav">
-          <li class="nav-item{% if path|starts_with:"/bloggers.md" %}
-            deltagande
-            {% endif %}"<a class="nav-link text" href="/bloggers.html{{lang}}">Ikonokloster</a></li>
-          <li class="nav-item{% if path|starts_with:"/contact.md" %}
-             deltagande
-             {% endif %}"<a class="nav-link" href="/contact.html{{lang}}">Kontakt</a></li>
-          <li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" id="products" data-bs-toggle="dropdown" role="button">Produkter... <span class="caret"></span></a>
-            <ul class="dropdown-menu me-auto mb-2 mb-lg-0" aria-labelledby="products">
-              <li class="dropdown-item"><a class="nav-link text-dark" href="https://www.sunstarsys.com/orion/index.html{{ lang }}">Orion&trade; Wiki-plattformen Jamstack</a></li>
-              <li class="dropdown-item"><a class="nav-link text-dark"
-                href="https://www.sunstarsys.com/orion/plans.html{{ lang }}"Orion: Prissättningsplaner</a></li>
-            </ul>
-          </li>
+<!######## 
+        
 
-<li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" id="more" role="button" data-bs-toggle="dropdown">Mer... <span class="caret"></span></a>
-            <ul class="dropdown-menu me-auto mb-2 mb-lg-0" aria-labelledby="more">
-              <li class="dropdown-item">
-                <a class="nav-link text-dark" href="https://vcs.sunstarsys.com/viewvc/public/cms-sites/{{website}}/trunk/">Platskälla</a>
-              </li>
-              <li class="dropdown-item divider"></li>
-              <li class="dropdown-header text-dark">i18n</li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.en">Engelska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.es">Spanska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.de">Tyska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.fr">Franska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.ru">Ryska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.zh-TW">kinesiska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.he">Hebreiska</a></li>
-              <li class="dropdown-item"><a class="nav-link" href="{{path|dirname|append:"/"}}{{path|basename:0}}.html.sv">Svenska</a></li>
-              <li class="dropdown-item divider"></li>
-              <li class="dropdown-header text-dark">Taxonomier</li>
-              <li class="dropdown-item"><a class="nav-link text-dark" href="/categories/index.html{{lang}}">Kategorier</a></li>
-              <li class="dropdown-item"><a class="nav-link text-dark" href="/archives/index.html{{lang}}">Arkiv</a></li>
-            </ul>
-          </li>
+          ><!--#######             aktiv>            <###! ### ## ### ###! ##### ### ### #### ##### ##### ### ### ####! 
 
-<li class="nav-item{% if path|starts_with:"/powered-by.md" %}
-            deltagande
-            {% endif %}"<a class="nav-link" href="/powered-by.html{{lang}}">Drivs av...</a>
-          </li>
-        </ul>
-		</div>
-        <form id="search" action="/dynamic/search{% ifequal path|dirname "/" %}{% else %}{{ path|dirname
-            }}{% endifequal %}/" class="d-flex form-inline höger" method="Hämta"
-          <input type="hidden" name="lang" value="{{ lang }}" />
-          <input type="hidden" name="markdown_search" value="1" />
-          <input class="form-control me-2" type="text" name="regex"
-               placeholder="PCRE: Rekursiv sökning" value="{{ regex }}" />&nbsp;<button type="submit" name="submit" value="1" class="btn btn-outline-danger"><i class="fa fa-search"></i></button>
-	    </form>
-  </div>
-</header>
+######## <!#### ##### <!#### ##### <!######### # <!--######## <!###### ####### ##### ###### ##### ##### ##### ##### ###### ##### ###### ######## ###### ###### ###### ##### ###### ##### ######## #######:######## ###### ####### ######## ####### ######### ########## ######## ########## ############ 
 
-{% block alert %}
-  {% if alert %}
-  <div class="alert alert-dismissible alert-info container">
-    <button type="button" class="btn-close" data-bs-toggle="modal" data-bs-target="#date-filter-modal"></button>
-    {{ alert|markdown }}
-  </div>
+<!######## 
+            aktiv
+            ""<!##### <!--##### #### ####!-- ###### ##### <!--###### ##### ###### ###### ####### ##### #### ####### ######!#### ####### ####### ###### ##### ####### ######## ####### ########### ########## ######### ############# ########### ########### ############# ######################### ############# ############################ ################### ####### ########### ###################### ########################## ################# ################ ###################### ############################# ############################## ####################### ##################################### ################################################### ############################################################# ################################################# ############################################################ ############################################################################################################################################################################################ 
+
+{% endif %}
   {% endif %}
-{% endblock %}
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav"><li class="nav-item{% if path|starts_with:"/bloggers.md" %}
+    {{ lang }}
+  <a class="nav-link text" href="/bloggers.html{{lang}}">
+  {% endif %}
+{% endifequal %}
 
-<div class="container theme-showcase" id="content">
-  {% block content %}
-  <div class="breadcrumbs">
-      {{ breadcrumbs|safe }}nbsp;&nbsp;<a href="javascript:void(location.href='https://cms.sunstarsys.com/redirect?uri='+escape(location.href))">
-        <img src="/images/edit.png" alt="Edit Icon" />
+</a>
+  {{ regex }}
+  </li>
+      {% block alert %}&trade;<li class="nav-item{% if path|starts_with:"/contact.md" %}
+        <a class="nav-link" href="/contact.html{{lang}}">
       </a>
-  </div>
-  <h1>{{ headers.title|safe }}</h1>
-  <div class="jumbotron">{{ content|markdown }}</div>
-  {% endblock %}
+  </li>
+  <li class="nav-item dropdown">{% if alert %}<a href="#" class="nav-link dropdown-toggle" id="products" data-bs-toggle="dropdown" role="button">
+  <span class="caret">{{ alert|markdown }}</span>
+  {% endif %}
 
-<footer>{% block footer %}{% endblock footer %}</footer><!--
-  <script src="/editor.md/js/raphael.min.js"></script>
-  <script src="/editor.md/js/underscore.min.js"></script>
-  <script src="/editor.md/js/flowchart.min.js"></script>
-  <script src="/editor.md/js/jquery.flowchart.min.js"></script>
-  <script src="/editor.md/js/sequence-diagram.min.js"></script> -->
+</a>{% endblock %}{% block content %}<ul class="dropdown-menu me-auto mb-2 mb-lg-0" aria-labelledby="products"><li class="dropdown-item">
+  <a class="nav-link text-dark" href="https://www.sunstarsys.com/orion/index.html{{ lang }}">
+  </a>
+  </li>
+  <li class="dropdown-item">
+  <a class="nav-link text-dark"
 
-<script src="/editor.md/js/d3.min.js"></script>
-  <script src="/editor.md/js/wasm/index.min.js"></script>
-  <script src="/editor.md/js/d3-graphviz.js"></script>
-  <script src="/editor.md/lib/mermaid.min.js"></script>
-  <script src="/editor.md/lib/codemirror/codemirror.min.js"></script>
-  <script src="/editor.md/lib/codemirror/addons.min.js"></script>
-  <script src="/editor.md/lib/codemirror/modes.min.js"></script>
-  <script src="/editor.md/lib/copy-tex.js"></script>
-  <script blocking="render" type="text/javascript">
-    if (typeof(editormd) === "undefined") {
-        //mermaid.registerExternalDiagrams([window["mermaid-mindmap"]]);
-        mermaid.initialize({theme: "light", startOnLoad: true, securityLevel: "loose"});
-        //$(".flowchart").flowChart();
-        //$(".sequence-diagram").sequenceDiagram();
-        for (const e of $("body").find(".graphviz").toArray()) {
-            d3.select(e).graphviz({useWorker: false}).renderDot($(e).text());
-            e.innerHTML = ""
-        }
-        $("body").find("pre").parent().addClass("editormd-preview-theme-light");
-        CodeMirror.colorize();
-    }
-
-if (document.cookie.indexOf("gdpr_analytics=1") == -1 &&
-    document.cookie.indexOf("gdpr_decline=1") == -1) {
-        for (const h1 of document.getElementsByTagName("h1")) {
-            var html = `<div id="analytics"><br><div class="card border-warning">
-<div class="card-header">
-  <h3 class="card-title text-dark">This Site Uses Cookies for Analytics.</h4>
-</div>
-<div class="card-body">
-<p class="card-text">
-<small class="text-dark">Please choose your Analytics preference:</small><br>
-  <button type="button" class="btn btn-outline-warning text-dark" data-bs-toggle="modal" data-bs-target="#date-filter-modal"
-  onClick="document.cookie='gdpr_analytics=1; path=/; max-age=8640000';
-  $('#analytics').css('display', 'none');true">I Consent.</button> &nbsp;
-  <button type="button" class="btn btn-outline-danger text-dark" data-bs-toggle="modal" data-bs-target="#date-filter-modal"
-  onClick="document.cookie='gdpr_decline=1; path=/; max-age=864000';
-  $('#analytics').css('display', 'none');true">I
-  Decline.</button><br><small class="text-dark">Should you elect to
-  Decline, we will not ask again for the next 10 days.</small>
-</p>
-</div>
-</div>
-</div>`;
-            h1.insertAdjacentHTML('beforeend', html);
-        }
-    }
-
-else if (document.cookie.indexOf("gdpr_decline=1") == -1) {
-        document.cookie = 'gdpr_analytics=1; path=/; max-age=8640000';
-    }
-  </script>
-  <script async type="module">
-    if (document.cookie.indexOf("can_search") >= 0 && Notification.permission !== "denied") {
-		var permission = Notification.permission;
-		if (permission !== "granted") {
-            Notification.requestPermission().then((result) => {
-              permission = result;
-            });
-        }
-
-if (permission === "granted") {
-		   var revision;
-           var m = document.cookie.match(/last=([0-9]+)/);
-           if (m)
-			 revision = m[1];
-           const response = await fetch("/dynamic/search/?regex=notify="+revision+";lang={{lang}};markdown_search=1;as_json=1",
-                           {credentials: 'same-origin'});
-           try {
-              const json = await response.json();
-              for (const e of json.log) {
-                  var msg = e[3] + "\n";
-                  for (const [key, val] of Object.entries(e[1])) {
-                      msg += val.action + " " + key.replace(/^.*\//, "") + "\n";
-				  }
-				  var n = new Notification(e[2],
-     			    {
-					  body: msg,
-					  tag: e[0],
-					  icon: "/images/favicon",
-					  image: "/images/iconoclast",
-				    }
-			  	  );
-			      n.addEventListener("click", () => {window.open("https://{{website}}/dynamic/search/?regex=diff="+e[0]+";lang={{lang}};markdown_search=1") }, { capture: true });
-			  }
-		   }
-           catch (e) {
-              alert(e);
-		   }
-        }
-	}
-  </script>
-  {% block javascript %}{% endblock %}
-</div>
-</body>
-</html>
+</a>
+  </li>
+  </ul>
+  </li>
+  <li class="nav-item dropdown">
+  <a href="#" class="nav-link dropdown-toggle" id="more" role="button" data-bs-toggle="dropdown">
+  <span class="caret">
+  </span>
+  </a>
+  <ul class="dropdown-menu me-auto mb-2 mb-lg-0" aria-labelledby="more">
+  {{ breadcrumbs|safe }}{{ headers.title|safe }}
+<li class="dropdown-item">
+<a class="nav-link text-dark" href="https://vcs.sunstarsys.com/viewvc/public/cms-sites/{{website}}/trunk/">
+</a>
