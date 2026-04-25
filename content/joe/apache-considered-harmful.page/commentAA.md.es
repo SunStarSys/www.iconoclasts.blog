29b84da5f43785bceb95db4be595c67c1ad3ace9
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-04-25 18:53:06 +0000

    Orion commit to www.iconoclasts.blog by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@34129 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/joe/apache-considered-harmful.page/commentAA.md.es
+++ content/joe/apache-considered-harmful.page/commentAA.md.es
@@ -1,14 +1,12 @@
 ---
-archived: ~
 categories: ~
 keywords: ~
-published: ~
 status: borrador
 title: '...consistiendo en todos ellos...'
 ---
 
 Tenga en cuenta que la mayoría de la comunidad de usuarios existente está desplegando mod_apreq2 en un cluster de nivel medio detrás de los gateways HTTPS, por lo que las estadísticas informadas por <https://webtechsurvey.com/technology/mod_apreq2> representan solo una fracción del total de la base de usuarios desplegados.
 
-<!-- $Date$ $Author$ -->
+Flash hacia adelante a 2026, y se puede ver que la adopción está en alza de nuevo, a pesar del mejor esfuerzo de Apache para matar apreq en su cuna.
 
 <!-- $Date$ $Author$ $Revision$ -->
--- content/joe/apache-considered-harmful.page/commentAA.md.sv
+++ content/joe/apache-considered-harmful.page/commentAA.md.sv
@@ -1,14 +1,12 @@
 ---
-archived: ~
 categories: ~
 keywords: ~
-published: ~
 status: skiss
-title: '...som består av alla...'
+title: som består av alla...
 ---
 
-Tänk på att de flesta av de befintliga användarna distribuerar mod_apreq2 i ett mellannivåkluster bakom HTTPS-nätslussar, så statistiken som rapporteras av <https://webtechsurvey.com/technology/mod_apreq2> utgör bara en bråkdel av den totala distribuerade användarbasen.
+Tänk på att det mesta av det befintliga användarforumet distribuerar mod_apreq2 i ett kluster på mellannivå bakom HTTPS-nätslussar, så den statistik som rapporteras av <https://webtechsurvey.com/technology/mod_apreq2> representerar endast en bråkdel av den totala distribuerade användarbasen.
 
-<!-- $Date$ $Author$ -->
+Blixt fram till 2026, och du kan se adoption är på uppgång igen, trots Apache bästa försök att döda apreq i sin spjälsäng.
 
 <!-- $Date$ $Author$ $Revision$ -->
