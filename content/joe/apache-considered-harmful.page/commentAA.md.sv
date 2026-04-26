a16c6f3c8c575560ecd63658ce710c660da75e82
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-04-26 16:04:12 +0000

    Orion commit to www.iconoclasts.blog by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@34176 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/joe/apache-considered-harmful.page/commentAA.md.sv
+++ content/joe/apache-considered-harmful.page/commentAA.md.sv
@@ -7,6 +7,6 @@ title: som består av alla...
 
 Tänk på att det mesta av det befintliga användarforumet distribuerar mod_apreq2 i ett kluster på mellannivå bakom HTTPS-nätslussar, så den statistik som rapporteras av <https://webtechsurvey.com/technology/mod_apreq2> representerar endast en bråkdel av den totala distribuerade användarbasen.
 
-Blixt fram till 2026, och du kan se adoption är på uppgång igen, trots Apache bästa försök att döda apreq i sin spjälsäng.
+Men blinka fram till 2026, och du kan se offentlig adoption är på uppgång igen, trots Apache bästa försök att döda apreq i sin spjälsäng.
 
 <!-- $Date$ $Author$ $Revision$ -->
