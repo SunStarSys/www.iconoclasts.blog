96c746c247c735bbf9c5cc27a41a96acf01eedca
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-05-08 07:08:00 +0000

    Orion commit to www.iconoclasts.blog by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.iconoclasts.blog/trunk@36873 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -38,7 +38,7 @@ our @patterns = (
 
   [qr!\.page/comment\w+\.md[^/]*$!, snippet => {
     template      => "comment.html.en",
-    view          => [qw/comment/],
+    view          => [qw/titleize_links comment/],
     archive_root  => "/archives",
     category_root => "/categories",
     facts         => $facts,
@@ -67,7 +67,7 @@ our @patterns = (
   }],
 
   [qr!^/(categories|archives)/.*\.md[^/]*!, set_template_from_capture => {
-    view          => [qw/ssi snippet offline titleize_links single_narrative/],
+    view          => [qw/ssi snippet single_narrative/],
     category_root => "/categories",
     facts         => $facts,
   }],
